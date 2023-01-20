c283a596d70e3a44666ea1cf4c8cc46758e586c3
Simon <83026016+simharuk2021@users.noreply.github.com>
2023-01-20 14:29:41 +0000

    sidebar slide in implemented for index page

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
-// toggles the display from none to block based on a conditional
-
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
+// toggles the display from none to block based on a conditional - used to render the menu when the hamburger icon is clicked
+function toggleNav() {
+    var nav= document.getElementById("mySidenav");
+    if (nav.style.width == "250px") {
+      nav.style.width = "0px";
     } else {
-      x.style.display = "block";
+      nav.style.width = "250px";
     }
   }
 
+  //function which passes the header text to the banner overlay and applys a typing style when the page is loaded
 var i = 0;
 var txt = 'My name is Simon Hart'; /* The text */
 var speed = 150; /* The speed/duration of the effect in milliseconds */
@@ -20,3 +20,21 @@ window.onload = function textType() {
     setTimeout(textType, speed);
   }
 }
+
+// /* Set the width of the side navigation to 250px */
+// function openNav() {
+//   document.querySelector("sidebar").style.width = "250px";
+// }
+
+// /* Set the width of the side navigation to 0 */
+// function closeNav() {
+//   document.querySelector("sidebar").style.width = "0px";
+// }
+
+// function openNav() {
+//   document.getElementById("mySidenav").style.width = "250px";
+// }
+
+// function closeNav() {
+//   document.getElementById("mySidenav").style.width = "0";
+// }
