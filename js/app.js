2770aa4810086f89f5f5dc6137dd7f8ff9c559c5
Simon <83026016+simharuk2021@users.noreply.github.com>
2023-01-21 07:55:48 +0000

    js fixes

--- js/app.js
+++ js/app.js
@@ -2,9 +2,9 @@
 
 function myFunction() {
     var x = document.querySelector(".sidebar-wrapper");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-     } else {
+    if (x.style.display === "block") {
       x.style.display = "none";
+     } else {
+      x.style.display = "block";
     }
   }
