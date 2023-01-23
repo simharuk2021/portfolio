3029c61a28c061059d7e0e30db86cf6285cdb2bb
Simon <83026016+simharuk2021@users.noreply.github.com>
2023-01-23 11:52:13 +0000

    burger corrections

--- js/app.js
+++ js/app.js
@@ -4,7 +4,13 @@ function myFunction() {
     var x = document.querySelector(".sidebar-wrapper");
     if (x.style.display === "block") {
       x.style.display = "none";
+      
      } else {
       x.style.display = "block";
+      document.querySelector("#burger").style.display = "none";
     }
   }
+function closeButton() {
+  document.querySelector(".sidebar-wrapper").style.display="none";
+  document.querySelector("#burger").style.display = "block";
+}
