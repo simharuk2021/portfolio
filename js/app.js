d9dd8453c1e5e5728937181e30f322f15f2ae26c
Simon <83026016+simharuk2021@users.noreply.github.com>
2023-01-26 11:02:46 +0000

    text typing implemented

--- js/app.js
+++ js/app.js
@@ -101,3 +101,17 @@ const validateInputs = () => {
     setSuccess(bigmessage);
   }
 };
+
+
+var i = 0;
+var txt = 'My name is Simon Hart'; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("textTyping").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+typeWriter();
